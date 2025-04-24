68e0484542cbfca8fdf52506ea08cd5bb9879280
William Jones <billy@freshjones.com>
2025-04-24 09:00:11 -0400

    loggin

--- src/resources/js/components/html-field.vue
+++ src/resources/js/components/html-field.vue
@@ -49,6 +49,8 @@ export default {
         },
     },
     mounted() {
+        console.log('hello', this.$refs.codemirror);
+
         this.codemirror = CodeMirror(this.$refs.codemirror, {
             value: this.getValue,
             mode: this.config.mode,
